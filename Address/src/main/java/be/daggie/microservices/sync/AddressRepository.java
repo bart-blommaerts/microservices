2a59ea15612412ce36547eafc5bffb564afe24fb
bart.blommaerts <bart.blommaerts@hp.com>
2014-09-07 21:09:30 +0200

    Read file on every request (for demo purposes)

--- Address/src/main/java/be/daggie/microservices/sync/AddressRepository.java
+++ Address/src/main/java/be/daggie/microservices/sync/AddressRepository.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import be.daggie.microservices.sync.Address;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class AddressRepository {
@@ -14,24 +16,33 @@ public class AddressRepository {
 	private Map<Long, Address> addressMap;
 
 	public AddressRepository() {
-		ObjectMapper mapper = new ObjectMapper();
 		addressMap = new HashMap<Long, Address>();
-
-		try {
-			List<Address> addresses = Arrays.asList(mapper.readValue(new File("/home/bart/files/addresses.json"), Address[].class));
-			for (Address address : addresses) {
-				putAddress(address);
-			}
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	public Address getAddress(Long id) {
+		convertListToMap(getCurrentAddressesFromFile());
+		
 		return addressMap.get(id);
 	}
 
+	private List<Address> getCurrentAddressesFromFile() {
+		ObjectMapper mapper = new ObjectMapper();
+		List<Address> addresses = null;
+		try {
+			addresses = Arrays.asList(mapper.readValue(new File("/home/bart/files/addresses.json"), Address[].class));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return addresses;
+	}
+	
+	private void convertListToMap(List<Address> addresses) {
+		for (Address address : addresses) {
+			putAddress(address);
+		}
+	}
+	
 	private void putAddress(Address p) {
 		addressMap.put(p.getId(), p);
 	}
